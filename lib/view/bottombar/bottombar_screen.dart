ec525c82488189080b1d0d61778821463011197d
harshil-micrasol <harshil.micrasol@gmail.com>
2024-05-29 09:23:18 +0530

    Add AnimatedClipRect widget in bottombar

--- lib/view/bottombar/bottombar_screen.dart
+++ lib/view/bottombar/bottombar_screen.dart
@@ -7,7 +7,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
 import '../../exports.dart';
-
+import '../../packages/app_animated_cliprect.dart';
 import 'bottombar_controller.dart';
 
 class BottomBarScreen extends StatelessWidget {
@@ -27,111 +27,111 @@ class BottomBarScreen extends StatelessWidget {
         }
         return false;
       },
-      child: Obx(() => Scaffold(
-            body: con.isLoading.value
-                ? Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: ListView.separated(
-                      itemCount: 15,
-                      separatorBuilder: (context, index) {
-                        return const SizedBox(height: 16);
-                      },
-                      itemBuilder: (context, index) {
-                        return ShimmerUtils.shimmer(
-                          child: ShimmerUtils.shimmerContainer(
-                            height: 70,
-                            width: Get.width,
-                            borderRadius: BorderRadius.circular(defaultRadius),
-                          ),
-                        );
-                      },
-                    ),
-                  )
-                : PageTransitionSwitcher(
-                    reverse: con.isLoggedIn.value,
-                    transitionBuilder: (
-                      Widget child,
-                      Animation<double> animation,
-                      Animation<double> secondaryAnimation,
-                    ) {
-                      return SharedAxisTransition(
-                        animation: animation,
-                        secondaryAnimation: secondaryAnimation,
-                        transitionType: con.transitionType!,
-                        fillColor: const Color(0x00FFFFFF),
-                        child: child,
+      child: Obx(
+        () => Scaffold(
+          body: con.isLoading.value
+              ? Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: ListView.separated(
+                    itemCount: 15,
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(height: 16);
+                    },
+                    itemBuilder: (context, index) {
+                      return ShimmerUtils.shimmer(
+                        child: ShimmerUtils.shimmerContainer(
+                          height: 70,
+                          width: Get.width,
+                          borderRadius: BorderRadius.circular(defaultRadius),
+                        ),
                       );
                     },
-                    child: con.bottomBarDataList.isNotEmpty ? con.bottomBarDataList[con.currentBottomIndex.value].screenWidget! : const SizedBox(),
                   ),
-            bottomNavigationBar: Obx(
-              () => con.bottomBarDataList.length >= 2
-                  ? IntrinsicHeight(
-                      child: Container(
-                        color: Theme.of(context).scaffoldBackgroundColor,
-                        child: BottomAppBar(
-                          notchMargin: 6,
-                          padding: EdgeInsets.symmetric(vertical: defaultPadding / 1.2),
-                          shape: const CircularNotchedRectangle(),
-                          child: Obx(
-                            () => Row(
-                              children: List.generate(
-                                con.bottomBarDataList.length,
-                                (index) => Expanded(
-                                  child: InkWell(
-                                    overlayColor: MaterialStateProperty.all(Colors.transparent),
-                                    onTap: () {
-                                      con.onBottomBarTap(index, hapticFeedback: true);
-                                    },
-                                    child: Center(
-                                      child: Column(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: [
-                                          AnimatedContainer(
-                                            duration: const Duration(milliseconds: 250),
-                                            padding: EdgeInsets.all(defaultPadding / 1.5),
-                                            decoration: BoxDecoration(
-                                              color: con.currentBottomIndex.value == index ? Theme.of(context).primaryColor : Theme.of(context).scaffoldBackgroundColor,
-                                              border: Border.all(color: con.currentBottomIndex.value == index ? Theme.of(context).primaryColor : Theme.of(context).scaffoldBackgroundColor, strokeAlign: 5),
-                                              shape: BoxShape.circle,
-                                            ),
-                                            child: con.currentBottomIndex.value == index
-                                                ? (isValEmpty(con.bottomBarDataList[index].bottomItem?.selectedImage)
-                                                    ? Icon(
-                                                        con.bottomBarDataList[index].bottomItem?.selectedIcon,
-                                                        size: con.bottomBarDataList[index].bottomItem?.selectedSize,
-                                                        color: AppColors.lightSecondary,
-                                                      )
-                                                    : SvgPicture.asset(
-                                                        con.bottomBarDataList[index].bottomItem?.selectedImage ?? "",
-                                                        height: con.bottomBarDataList[index].bottomItem?.selectedSize ?? 21,
-                                                        color: AppColors.lightSecondary,
-                                                      ))
-                                                : isValEmpty(con.bottomBarDataList[index].bottomItem?.selectedImage)
-                                                    ? Icon(
-                                                        con.bottomBarDataList[index].bottomItem?.unselectIcon ?? con.bottomBarDataList[index].bottomItem?.selectedIcon,
-                                                        size: con.bottomBarDataList[index].bottomItem?.unselectSize,
-                                                        color: Theme.of(context).primaryColor.withOpacity(.9),
-                                                      )
-                                                    : SvgPicture.asset(
-                                                        con.bottomBarDataList[index].bottomItem?.unselectImage ?? con.bottomBarDataList[index].bottomItem?.selectedImage ?? "",
-                                                        height: con.bottomBarDataList[index].bottomItem?.unselectSize ?? 21,
-                                                        color: Theme.of(context).primaryColor.withOpacity(.9),
-                                                      ),
+                )
+              : PageTransitionSwitcher(
+                  reverse: con.isLoggedIn.value,
+                  transitionBuilder: (
+                    Widget child,
+                    Animation<double> animation,
+                    Animation<double> secondaryAnimation,
+                  ) {
+                    return SharedAxisTransition(
+                      animation: animation,
+                      secondaryAnimation: secondaryAnimation,
+                      transitionType: con.transitionType!,
+                      fillColor: const Color(0x00FFFFFF),
+                      child: child,
+                    );
+                  },
+                  child: con.bottomBarDataList.isNotEmpty ? con.bottomBarDataList[con.currentBottomIndex.value].screenWidget! : const SizedBox(),
+                ),
+          bottomNavigationBar: Obx(
+            () => con.bottomBarDataList.length >= 2
+                ? IntrinsicHeight(
+                    child: Container(
+                      color: Theme.of(context).scaffoldBackgroundColor,
+                      child: BottomAppBar(
+                        notchMargin: 6,
+                        padding: EdgeInsets.symmetric(vertical: defaultPadding / 1.2),
+                        shape: const CircularNotchedRectangle(),
+                        child: Obx(
+                          () => Row(
+                            children: List.generate(
+                              con.bottomBarDataList.length,
+                              (index) => Expanded(
+                                child: InkWell(
+                                  overlayColor: MaterialStateProperty.all(Colors.transparent),
+                                  onTap: () {
+                                    con.onBottomBarTap(index, hapticFeedback: true);
+                                  },
+                                  child: Center(
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        AnimatedContainer(
+                                          duration: const Duration(milliseconds: 250),
+                                          padding: EdgeInsets.all(defaultPadding / 1.5),
+                                          decoration: BoxDecoration(
+                                            color: con.currentBottomIndex.value == index ? Theme.of(context).primaryColor : Theme.of(context).scaffoldBackgroundColor,
+                                            border: Border.all(color: con.currentBottomIndex.value == index ? Theme.of(context).primaryColor : Theme.of(context).scaffoldBackgroundColor, strokeAlign: 5),
+                                            shape: BoxShape.circle,
                                           ),
-                                          AnimatedDefaultTextStyle(
-                                            duration: defaultDuration,
+                                          child: con.currentBottomIndex.value == index
+                                              ? (isValEmpty(con.bottomBarDataList[index].bottomItem?.selectedImage)
+                                                  ? Icon(
+                                                      con.bottomBarDataList[index].bottomItem?.selectedIcon,
+                                                      size: con.bottomBarDataList[index].bottomItem?.selectedSize,
+                                                      color: AppColors.lightSecondary,
+                                                    )
+                                                  : SvgPicture.asset(
+                                                      con.bottomBarDataList[index].bottomItem?.selectedImage ?? "",
+                                                      height: con.bottomBarDataList[index].bottomItem?.selectedSize ?? 21,
+                                                      color: AppColors.lightSecondary,
+                                                    ))
+                                              : isValEmpty(con.bottomBarDataList[index].bottomItem?.selectedImage)
+                                                  ? Icon(
+                                                      con.bottomBarDataList[index].bottomItem?.unselectIcon ?? con.bottomBarDataList[index].bottomItem?.selectedIcon,
+                                                      size: con.bottomBarDataList[index].bottomItem?.unselectSize,
+                                                      color: Theme.of(context).primaryColor.withOpacity(.9),
+                                                    )
+                                                  : SvgPicture.asset(
+                                                      con.bottomBarDataList[index].bottomItem?.unselectImage ?? con.bottomBarDataList[index].bottomItem?.selectedImage ?? "",
+                                                      height: con.bottomBarDataList[index].bottomItem?.unselectSize ?? 21,
+                                                      color: Theme.of(context).primaryColor.withOpacity(.9),
+                                                    ),
+                                        ),
+                                        AnimatedClipRect(
+                                          open: con.currentBottomIndex.value != index,
+                                          child: Text(
+                                            con.bottomBarDataList[index].screenName ?? "",
                                             style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                                                  color: con.currentBottomIndex.value == index ? AppColors.secondary : Theme.of(context).primaryColor.withOpacity(.9),
-                                                  fontSize: con.currentBottomIndex.value != index ? 9.sp : 0,
+                                                  color: Theme.of(context).primaryColor.withOpacity(.9),
+                                                  fontSize: 9.sp,
                                                   fontWeight: FontWeight.w600,
                                                 ),
-                                            child: Text(
-                                              con.bottomBarDataList[index].screenName ?? "",
-                                            ),
                                           ),
-                                        ],
-                                      ),
+                                        ),
+                                      ],
                                     ),
                                   ),
                                 ),
@@ -140,10 +140,12 @@ class BottomBarScreen extends StatelessWidget {
                           ),
                         ),
                       ),
-                    )
-                  : const SizedBox(),
-            ),
-          )),
+                    ),
+                  )
+                : const SizedBox(),
+          ),
+        ),
+      ),
     );
   }
 }
