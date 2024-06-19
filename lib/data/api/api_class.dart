9c3ad3b8151af834123f753d92b0c52ec6ae4510
dishankmicrasol <dishank.micrasol@gmail.com>
2024-06-19 12:59:38 +0530

    firebase setup completed

--- lib/data/api/api_class.dart
+++ lib/data/api/api_class.dart
@@ -4,7 +4,6 @@ import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 import '../../exports.dart';
